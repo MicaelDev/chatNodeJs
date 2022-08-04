d5b772081470796d36d927395a9e51fa8aba232c
MicaelDev <micaelquiliao20@gmail.com>
2022-08-03 23:38:53 -0300

    First test with socket.on and socket.emit (chat users in and out)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const socketIO = require('socket.io');
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,8 +9,33 @@ const io = socketIO(server);
 
 server.listen(3000);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection', (socket) => {
-    console.log("Conexão detectada...");
+let connectedUsers = [];
+
+io.on("connection", (socket) => {
+  console.log("Conexão detectada...");
+
+  socket.on("join-request", (username) => {
+    socket.username = username;
+    connectedUsers.push(username);
+    console.log(connectedUsers);
+
+    socket.emit("user-ok", connectedUsers);
+    socket.broadcast.emit('list-update', {
+      joined: username,
+      list: connectedUsers
+    });
+  });
+
+  socket.on("disconnect", () => {
+      connectedUsers = connectedUsers.filter(u => u != socket.username);
+      console.log(connectedUsers);
+
+      socket.broadcast.emit('list-update', {
+        left: socket.username,
+        list: connectedUsers
+      });
+  });
+
 });
