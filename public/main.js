25a13bbce4d97f82845abfbb243d53c7192f036a
MicaelDev <micaelquiliao20@gmail.com>
2022-08-17 23:13:49 -0300

    ...continuing the project

--- public/main.js
+++ public/main.js
@@ -20,6 +20,19 @@ function renderUserList() {
   });
 }
 
+function addMessage(type, user, msg){
+  let ul = document.querySelector('.chatList');
+
+  switch(type){
+    case 'status':
+        ul.innerHTML += '<li class="m-status">'+msg+'</li>';
+      break;
+      case 'msg':
+        ul.innerHTML += '<li class="m-txt"><span>'+user+'</span> '+msg+'</li>';
+      break;
+  }
+}
+
 loginInput.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
     let name = loginInput.value.trim();
@@ -32,16 +45,41 @@ loginInput.addEventListener("keyup", (e) => {
   }
 });
 
+textInput.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    let txt = textInput.value.trim();
+    textInput.value = "";
+
+    if (txt != "") {
+      socket.emit('send-msg', txt);
+    }
+  }
+});
+
 socket.on("user-ok", (list) => {
   loginPage.style.display = "none";
   chatPage.style.display = "flex";
   textInput.focus();
+
+  addMessage('status', null, 'Conectado!');
 
   userList = list;
   renderUserList();
 });
 
 socket.on('list-update', (data) => {
+  if (data.joined) {
+    addMessage('status', null, data.joined+' entrou no chat.');
+  }
+  
+  if (data.left) {
+    addMessage('status', null, data.left+' saiu do chat.');
+  }
+
   userList = data.list;
   renderUserList();
+});
+
+socket.on('show-msg', (data) => {
+  addMessage('msg', data.username, data.message);
 });
